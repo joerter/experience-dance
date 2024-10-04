d534c07569c9362c8e79cb56b33889a3c9ba7ddf
John Oerter <joerter@dinacare.com>
2024-10-03 21:37:40 -0400

    Create permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class)->withTimestamps();
+    }
 }
