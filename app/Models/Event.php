efcf6228ebd197264d7c2736b7cf709baa97312f
John Oerter <joerter@dinacare.com>
2024-10-03 20:05:20 -0400

    Create category

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Event extends Model
 {
@@ -24,4 +25,9 @@ class Event extends Model
         'email',
         'phone',
     ];
+
+    public function categories(): HasMany
+    {
+        return $this->hasMany(Category::class);
+    }
 }
