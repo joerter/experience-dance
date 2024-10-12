5de911a87e2407f5e2a2e17636ca710273c0dd72
John Oerter <joerter@dinacare.com>
2024-10-12 10:58:05 -0400

    Responsive side nav

--- resources/js/Components/NavBar.tsx
+++ resources/js/Components/NavBar.tsx
@@ -1,6 +1,37 @@
-import { AppBar, Box, Button, Stack, Toolbar } from "@mui/material";
+import { AppBar, Box, Button, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText, Stack, Toolbar } from "@mui/material";
+import { useState } from "react";
+
+const navItems = [
+  { text: 'Home', href: '/' },
+  { text: 'Events', href: '/events' },
+  { text: 'About', href: '/about' },
+  { text: 'Contact', href: '/contact' },
+]
+
+function SideNav({ onToggleSideNav }: { onToggleSideNav: () => void }) {
+  return (
+    <Box onClick={onToggleSideNav} sx={{ px: 6 }}>
+      <List>
+        {navItems.map((item, i) => (
+          <ListItem key={i} disablePadding sx={{ borderBottom: 1, borderColor: 'primary.main' }}>
+            <ListItemButton sx={{ textAlign: 'center' }} href={item.href}>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  )
+}
+
 
 export default function NavBar() {
+  const [sideNavOpen, setSideNavOpen] = useState(false);
+
+  const handleToggleSideNav = () => {
+    setSideNavOpen((prevState) => !prevState);
+  };
+
   return (
     <Box sx={{ flexGrow: 1, p: 4 }}>
       <AppBar position="static" color="transparent">
@@ -9,18 +40,34 @@ export default function NavBar() {
           <Stack direction="row" spacing={2} sx={(theme) => ({
             p: 2,
             border: `1px solid ${theme.palette.gray2.main}`,
-            borderRadius: '30px'
+            borderRadius: '30px',
+            display: { xs: 'none', lg: 'flex' }
           })}>
-            <Button color="primary" variant="contained">Home</Button>
-            <Button color="inherit" variant="outlined">Events</Button>
-            <Button color="inherit" variant="outlined">About</Button>
-            <Button color="inherit" variant="outlined">Contact</Button>
+            {navItems.map((item, i) => (
+              <Button key={i} color="inherit" variant="outlined">{item.text}</Button>
+            ))}
           </Stack>
-          <Stack>
+          <Stack sx={{ display: { xs: 'none', lg: 'flex' } }}>
             <Button color="inherit" variant="outlined" sx={{ fontWeight: '600' }}>Submit Event</Button>
           </Stack>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            sx={{ display: { xs: 'block', lg: 'none' } }}
+            onClick={() => { handleToggleSideNav(); }}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height={24} width={24}>
+              <line x1="4" y1="6" x2="20" y2="6" stroke="white" stroke-width="2" stroke-linecap="round" />
+              <line x1="12" y1="12" x2="20" y2="12" stroke="white" stroke-width="2" stroke-linecap="round" />
+              <line x1="4" y1="18" x2="20" y2="18" stroke="white" stroke-width="2" stroke-linecap="round" />
+            </svg>
+          </IconButton>
         </Toolbar>
       </AppBar>
+      <Drawer open={sideNavOpen} onClose={() => { handleToggleSideNav() }} PaperProps={{ sx: { background: 'black', color: 'white' } }}>
+        <SideNav onToggleSideNav={handleToggleSideNav} />
+      </Drawer>
     </Box>
   )
 }
