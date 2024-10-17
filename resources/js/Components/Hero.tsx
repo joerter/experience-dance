56f677eb097c1a5385426f81ac0e8a5bcdc36cd1
John Oerter <joerter@dinacare.com>
2024-10-16 22:22:48 -0400

    Experiment with the marquee effect

--- resources/js/Components/Hero.tsx
+++ resources/js/Components/Hero.tsx
@@ -1,5 +1,41 @@
-import { Stack, Typography } from '@mui/material';
+import { keyframes, Stack, Typography } from '@mui/material';
 import EventSearch from './EventSearch';
+
+const featuredEvents: {
+  name: string;
+  organization: string;
+  location: string;
+}[] = [
+  {
+    name: 'The Long Black Veil',
+    organization: 'New England Ballet Theatre',
+    location: 'West Hartford, CT',
+  },
+  {
+    name: 'The Fantastic Toy Shop',
+    organization: 'New England Ballet Theatre',
+    location: 'West Hartford, CT',
+  },
+  {
+    name: 'Emerging Works',
+    organization: 'New England Ballet Theatre',
+    location: 'West Hartford, CT',
+  },
+  {
+    name: 'La Sylphide',
+    organization: 'New England Ballet Theatre',
+    location: 'West Hartford, CT',
+  },
+];
+
+const marquee = keyframes`
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(100%);
+  }
+`;
 
 export default function Hero() {
   return (
@@ -40,6 +76,48 @@ export default function Hero() {
           </Typography>
         </Stack>
         <EventSearch variant="dark" />
+      </Stack>
+
+      <Stack
+        direction="row"
+        alignItems="center"
+        sx={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          overflow: 'hidden',
+          animation: `${marquee} 60s linear infinite`,
+          '&:hover': {
+            animationPlayState: 'paused',
+          },
+        }}
+      >
+        {featuredEvents.map((fe) => (
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            sx={{ width: '300px', p: 2 }}
+          >
+            <Stack>
+              <Typography variant="body1">{fe.name}</Typography>
+              <Typography
+                variant="body2"
+                fontSize={(theme) => theme.typography.pxToRem(10)}
+              >
+                {fe.organization}
+              </Typography>
+              <Typography
+                variant="body2"
+                fontSize={(theme) => theme.typography.pxToRem(10)}
+              >
+                {fe.location}
+              </Typography>
+            </Stack>
+            <img height="24px" width="24px" src="/images/logo-small.svg" />
+          </Stack>
+        ))}
       </Stack>
     </Stack>
   );
