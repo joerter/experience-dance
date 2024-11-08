a41367eb6005fab8e7d6c0746914802bb62869b3
John Oerter <joerter@dinacare.com>
2024-11-08 08:11:16 -0500

    Style tweaking

--- resources/js/Components/EventSearch.tsx
+++ resources/js/Components/EventSearch.tsx
@@ -27,6 +27,17 @@ export default function EventSearch({
     DateRangeOption.Week,
   );
 
+  const textColor = variant === 'dark' ? 'text.primary' : 'text.secondary';
+  const borderColor =
+    variant === 'dark' ? 'rgba(255, 255, 255, .3)' : 'rgba(0, 0, 0, .3)';
+  const notchedOutlineStyles = {
+    '.MuiOutlinedInput-notchedOutline': {
+      borderRadius: 170,
+      border: `1px solid ${borderColor}`,
+      background: 'rgba(255, 255, 255, .1)',
+    },
+  };
+
   return (
     <Stack
       alignItems="flex-start"
@@ -38,22 +49,22 @@ export default function EventSearch({
         <FormControl size="small">
           <Select
             id="event-date-selector"
+            variant="outlined"
+            notched={false}
             value={dateRange}
             aria-label="Date"
             onChange={(event: SelectChangeEvent<DateRangeOption>) => {
               setDateRange(event.target.value as DateRangeOption);
             }}
             sx={{
-              color: 'text.primary',
-              '.MuiSelect-icon': { color: 'text.primary' },
+              color: textColor,
+              '.MuiSelect-icon': { color: textColor },
+              ...notchedOutlineStyles,
             }}
             IconComponent={ExpandMoreIcon}
             inputProps={{
               sx: {
-                color: 'text.primary',
-                borderRadius: 170,
-                border: '1px solid rgba(255, 255, 255, .3)',
-                background: 'rgba(255, 255, 255, .1)',
+                color: textColor,
               },
             }}
             MenuProps={{
@@ -78,31 +89,32 @@ export default function EventSearch({
         options={['Hartford', 'Omaha']}
         slotProps={{ paper: { sx: { color: 'text.secondary' } } }}
         sx={{
-          color: 'text.primary',
-          borderRadius: 170,
-          border: '1px solid rgba(255, 255, 255, .3)',
-          background: 'rgba(255, 255, 255, .1)',
+          color: textColor,
+          '.MuiSelect-icon': { color: textColor },
+          ...notchedOutlineStyles,
+          '.Mui-focused .MuiOutlinedInput-notchedOutline': {
+            borderColor: 'primary.main',
+          },
+          '&:hover .MuiOutlinedInput-notchedOutline': {
+            borderColor: 'primary.main',
+          },
         }}
         renderInput={(params) => (
           <TextField
             {...params}
             placeholder="Enter location..."
-            variant="standard"
+            variant="outlined"
             slotProps={{
               input: {
                 ...params.InputProps,
                 type: 'search',
-                sx: { color: 'text.primary', border: 'none' },
+                sx: { color: textColor, border: 'none' },
                 endAdornment: (
-                  <InputAdornment position="end" sx={{ color: 'text.primary' }}>
+                  <InputAdornment position="end" sx={{ color: textColor }}>
                     <SearchIcon color="inherit" />
                   </InputAdornment>
                 ),
               },
-            }}
-            sx={{
-              py: 1,
-              px: 2,
             }}
           />
         )}
