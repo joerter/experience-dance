0a8f6966a6767365d771af0a34de6a5603c0cef5
John Oerter <john@johnoerter.me>
2025-01-14 07:59:20 -0500

    Create LoginToken Mailable

--- config/mail.php
+++ config/mail.php
@@ -109,8 +109,9 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@experience.dance'),
+        'reply_to' => env('MAIL_FROM_ADDRESS', 'support@experience.dance'),
+        'name' => env('MAIL_FROM_NAME', 'Experience Dance'),
     ],
 
 ];
